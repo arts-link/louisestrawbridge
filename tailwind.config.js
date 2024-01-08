b93d605692072a2c9fb3f09f9d3c16c1b2ae43c4
ben strawbridge <ben@benstrawbridge.com>
2024-01-08 13:47:33 -0800

    update mailchimp form style

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms")
+  ],
 };
